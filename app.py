5b93d825770ff33ad19f1f92205b2cc1dd8adc8c
Anay Berde <anayberde14@gmail.com>
2026-04-22 23:32:11 -0700

    Implemented hashing for passwords stored in password column in mlklibrary.db

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import sqlite3
+import hashlib
+import secrets
 
 app = Flask(__name__)
 
@@ -8,11 +10,21 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        id = request.form['library_id']
+        plaintext = request.form['password']
+
+        conn = sqlite3.connect('mlklibrary.db') # connect route to database
+        cursor = conn.cursor()
+        val = cursor.execute("SELECT library_id FROM Users WHERE library_id = id")
+
 @app.route('/dashboard')
 def dashboard():
     conn = get_db_connection()
     rows = conn.execute(
-        "SELECT first_name || ' ' || last_name AS full_name, library_id, role FROM users"
+        "SELECT first_name || ' ' || last_name AS full_name, library_id, role FROM Users"
     ).fetchall()
     conn.close()
 
@@ -26,13 +38,16 @@ def create_user():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         library_id = request.form['library_id']
-        password = request.form['password']
         role = request.form['role']
 
+        plaintext = request.form['password']
+        salt = secrets.token_hex(8)
+        temp = hashlib.sha256((salt + plaintext).encode())
+        hashed_password = temp.hexdigest()
         conn = sqlite3.connect('mlklibrary.db') # connect route to database
         cursor = conn.cursor()
-        cursor.execute("""INSERT INTO Users (first_name, last_name, library_id, password, role) VALUES (?, ?, ?, ?, ?)"""
-                       ,(first_name, last_name, library_id, password, role)) # insert user-defined variables into database
+        cursor.execute("""INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?, ?)"""
+        ,(first_name, last_name, library_id, hashed_password, role, "active", salt)) # insert user-defined variables into database
         conn.commit()
         conn.close()
     return render_template('create_user.html')
--- createdb.py
+++ createdb.py
@@ -1,4 +1,6 @@
 import sqlite3
+import hashlib
+import secrets
 
 conn = sqlite3.connect("mlklibrary.db")
 cursor = conn.cursor() # executes SQLite instructions
@@ -15,8 +17,29 @@ cursor = conn.cursor() # executes SQLite instructions
 # # cursor.execute("""INSERT INTO Users VALUES ("Bernie", "Kosar", "015667180", "*browns_football", "Professor")""")
 # cursor.execute("""INSERT INTO Users VALUES ("John", "Smith", "567001119", "iloveRe@ading", "Borrower")""")
 
-# STATEMENTS ABOVE ARE COMMENTED TO PREVENT NEW CREATIONS UPON FILE EXECUTION
+# cursor.execute("""ALTER TABLE Users ADD COLUMN active_status text default 'active'""")
 
-cursor.execute("""ALTER TABLE Users ADD COLUMN active_status text default 'active'""")
+# cursor.execute("""DROP TABLE Users""") # DELETES TABLE, WANTED TO RESET SO THAT DB STORES HASHED PASSWORDS
+
+# cursor.execute("""CREATE TABLE Users( # RECREATES TABLE
+#         first_name text,
+#         last_name text,
+#         library_id text,
+#         password text,
+#         role text,
+#         active_status text default 'active'
+#         )""")
+cursor.execute("""ALTER TABLE Users ADD COLUMN salt""") # MADE AN ERROR HERE, OOPS!
+cursor.execute("""ALTER TABLE Users DROP COLUMN salt""")
+cursor.execute("""ALTER TABLE Users ADD COLUMN salt text""")
+plaintext = "sjsu_librarian"
+salt = secrets.token_hex(8)
+temp = hashlib.sha256((salt + plaintext).encode())
+password = temp.hexdigest()
+
+cursor.execute("""INSERT INTO Users VALUES (?, ?, ?, ?, ?, ?, ?)"""
+, ("Librarian", "Librarian", "000000000", password, "Librarian", "active", salt))
+# STATEMENTS ABOVE ARE COMMENTED TO PREVENT UNINTENDED UPDATES UPON RUNNING PROGRAM
+
 conn.commit() #commits changes to our database
 conn.close()
