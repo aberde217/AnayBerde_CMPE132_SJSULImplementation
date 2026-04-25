436d9b07350413d6161469d5c7bf19561a75a97b
Anay Berde <anayberde14@gmail.com>
2026-04-25 09:21:38 -0700

    Login Page now supports secure authentication

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import sqlite3
 import hashlib
 import secrets
-
 app = Flask(__name__)
+app.secret_key = "mlkkeyanay"
 
 def get_db_connection():
     conn = sqlite3.connect('mlklibrary.db')
@@ -18,7 +18,29 @@ def login():
 
         conn = sqlite3.connect('mlklibrary.db') # connect route to database
         cursor = conn.cursor()
-        val = cursor.execute("SELECT library_id FROM Users WHERE library_id = id")
+        cursor.execute("SELECT * FROM Users WHERE library_id = (?)", (id,)) # selects ROW for that library id if it exists
+        row = cursor.fetchone()
+        if row is None:
+            flash('Invalid Credentials, login failed.', 'error')
+        else:
+            salt = row[6] # last column, which is salt
+            hash_obj = hashlib.sha256((salt + plaintext).encode())
+            hash_val = hash_obj.hexdigest()
+            if hash_val != row[3]: # compare entered password with stored (hashed/salted)
+                flash('Invalid Credentials, login failed', 'error')
+            elif row[5] == 'inactive':  # check if user is active
+                flash('Account deactivated. Visit MLK Library for further action.', 'error')
+            else:
+                conn = get_db_connection()
+                rows = conn.execute(
+                    "SELECT first_name || ' ' || last_name AS full_name, library_id, role FROM Users"
+                ).fetchall()
+                conn.close()
+
+                users = [dict(row) for row in rows]
+                return render_template('admin_dashboard.html', users=users)
+    return render_template('login.html')
+
 
 @app.route('/dashboard')
 def dashboard():
